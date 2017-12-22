705fae79798058fb8ed6fcef150702e1233bb159
lionpowder <hanna7810@hotmail.com>
2017-12-22 14:56:09 -0500

    Add files via upload

---
+++ BackEnd/GeoProjectWcf/GeoEntity/GeoEntityService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoEntity
+{
+    public class GeoEntityService
+    {
+        protected GeoWcfEntities entities { get; set; }
+
+        public GeoEntityService(GeoWcfEntities entities) 
+        {
+            this.entities = entities;
+        }
+
+        public int login(string username, string password, int issignup)
+        {
+            
+            // isSignUp => check username exists => if not exist, save to database
+            if (issignup == 1)
+            {
+                if (!entities.user.Any(a => a.username == username))
+                {
+                    user newUser = new user();
+                    newUser.username = username;
+                    newUser.psw = password;
+                    entities.user.Add(newUser);
+                    entities.SaveChanges();
+                    return 1;
+                }
+            }
+            else // SignIn => check both username and password
+            {
+                if (entities.user.Any(a => a.username == username))
+                {
+                    string dbPassword = entities.user.Where(a => a.username == username).Select(a => a.psw).FirstOrDefault();
+
+                    return dbPassword == password ? 1 : 0;
+                }
+            }
+
+            return 0;
+        }
+
+        public int SetFavorite(string username, string[][] idList, int isAdd)
+        {
+            List<favorite> selectedList = entities.favorite.Where(f => f.username == username).ToList();
+
+            // isAdd add single record
+            if (isAdd == 1)
+            {
+                if (!selectedList.Exists(f => f.locationName == idList[0][0]))
+                {
+                    favorite fav = new favorite();
+                    fav.username = username;
+                    fav.locationName = idList[0][0];
+                    fav.location = idList[0][1];
+
+                    entities.favorite.Add(fav);
+                    entities.SaveChanges();
+
+                    return 1;
+                }                
+            }
+            else // is not add, delete current old record and add new
+            {
+                entities.favorite.RemoveRange(selectedList);
+                entities.SaveChanges();
+
+                List<favorite> newList = new List<favorite>();
+
+                foreach (string[] ids in idList)
+                {
+                    favorite fav = new favorite();
+                    fav.username = username;
+                    fav.locationName = ids[0];
+                    fav.location = ids[1];
+
+                    newList.Add(fav);
+                }
+
+                entities.favorite.AddRange(newList);
+                entities.SaveChanges();
+
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public Dictionary<string, string> GetFavorite(string username)
+        {
+            Dictionary<string, string> favorites = new Dictionary<string, string>();
+
+            if (entities.favorite.Any(f => f.username == username))
+            {
+                List<favorite> selectedList = entities.favorite.Where(f => f.username == username).Select(f => f).ToList();
+
+                favorites = selectedList.ToDictionary(f => f.locationName, f => f.location);
+            }
+
+            return favorites;
+        }
+
+        public string GetCoord(string id)
+        {
+            return entities.idCoord.Any(i => i.id == id) ? 
+                entities.idCoord.Where(i => i.id == id).Select(i => i.coord).FirstOrDefault() : "not found";
+        }
+    }
+}
---
+++ BackEnd/GeoProjectWcf/GeoLib/CommonLogger.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging.ExtraInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib
+{
+    public class CommonLogger : LogWriterFactory
+    {
+        /* Logwriter 物件*/
+        private static LogWriter logWriter = new LogWriterFactory().Create();
+
+        /// <summary>
+        /// 記錄Information
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="msg"></param>
+        public static void LogInformation(object obj, string msg)
+        {
+            LogEntry log = LoggingFactory.GetInformationLogEntry(obj);
+            log.Message = msg;
+            Log(log);
+        }
+
+        /// <summary>
+        /// 記錄Error
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="msg"></param>
+        /// <param name="e"></param>
+        /// <param name="extendedProperties"></param>
+        /// <param name="extendedValue"></param>
+        public static void LogError(object obj, string msg, System.Exception e, Dictionary<string, string> extendedValue)
+        {
+            LogEntry log = LoggingFactory.GetErrorLogEntry(obj);
+            log.Message = msg;
+            log.ExtendedProperties.Add("Failure", e);
+
+            foreach (string property in extendedValue.Keys)
+            {
+                string va = "";
+                extendedValue.TryGetValue(property, out va);
+                log.ExtendedProperties.Add(property, va);
+            }
+
+            Log(log);
+        }
+
+        /// <summary>
+        /// 記錄Error
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="msg"></param>
+        /// <param name="e"></param>
+        public static void LogError(object obj, string msg, System.Exception e)
+        {
+            LogEntry log = LoggingFactory.GetErrorLogEntry(obj);
+            log.Message = msg;
+            log.ExtendedProperties.Add("Failure", e);
+            Log(log);
+        }
+
+        /// <summary>
+        /// 記錄Debug
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="msg"></param>
+        public static void LogDebug(object obj, string msg)
+        {
+            LogEntry log = LoggingFactory.GetDebugLogEntry(obj);
+            log.Message = msg;
+            Log(log);
+        }
+
+        private static void Log(LogEntry log)
+        {
+            AddExtendedProperty(log);
+            logWriter.Write(log);
+        }
+
+        private static void AddExtendedProperty(LogEntry log)
+        {
+            if (log.Severity == System.Diagnostics.TraceEventType.Error || log.Severity == System.Diagnostics.TraceEventType.Critical)
+            {
+                Dictionary<string, object> dictionary = new Dictionary<string, object>();
+                foreach (KeyValuePair<string, object> keyValue in log.ExtendedProperties)
+                {
+                    dictionary.Add(keyValue.Key, keyValue.Value);
+                }
+                DebugInformationProvider infoProvider = new DebugInformationProvider();
+
+                //呼叫DebugInformationProvider的PopulateDictionary方法，
+                //將debug訊息自動加載到dictionary中。
+                infoProvider.PopulateDictionary(dictionary);
+
+                log.ExtendedProperties = dictionary;
+            }
+        }
+    }
+}
